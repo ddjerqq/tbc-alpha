9486ccdac419c3ca92a9d28ff141bfbf3aeca14f
ddjerqq <57017344+ddjerqq@users.noreply.github.com>
2024-10-20 00:41:40 +0400

    fixed CurrencyExchange.cs

--- src/Application/Services/CurrencyExchange.cs
+++ src/Application/Services/CurrencyExchange.cs
@@ -1,30 +1,29 @@
 using Domain.Common;
 using Domain.ValueObjects;
-
-namespace Application.Services;
-
 using System.Net.Http.Json;
 using Microsoft.Extensions.Caching.Memory;
 
-public record CurrencyResponse(string Date, string Base, Dictionary<Currency, Currency> Rates)
-{
-}
+namespace Application.Services;
+
+public sealed record CurrencyResponse(Dictionary<Currency, Currency> Rates);
 
 public class CurrencyService(HttpClient client, IMemoryCache cache) : ICurrencyExchange
 {
-    private readonly Uri _uri =
-        new(
-            $"https://api.currencyfreaks.com/v2.0/rates/latest?apikey={"JWT__KEY".FromEnvRequired()}&symbols=GEL,EUR,USD,GBP");
+    private static readonly string Uri =
+        $"https://api.currencyfreaks.com/v2.0/rates/latest?" +
+        $"apikey={"CURRENCY_EXCHANGE__API_KEY".FromEnvRequired()}" +
+        $"&symbols=GEL,EUR,USD,GBP";
 
-    private static object _key = 1;
+    private static readonly object Key = 1;
 
     private async Task<CurrencyResponse> GetCurrencyExchange()
     {
-        if (!cache.TryGetValue(_key, out CurrencyResponse? currencyResponse))
+        if (!cache.TryGetValue(Key, out CurrencyResponse? currencyResponse))
         {
-            currencyResponse = await client.GetFromJsonAsync<CurrencyResponse>(_uri);
-            cache.Set(_key, currencyResponse,
-                new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromMinutes(5)));
+            currencyResponse = await client.GetFromJsonAsync<CurrencyResponse>(Uri);
+
+            var cacheOptions = new MemoryCacheEntryOptions().SetAbsoluteExpiration(TimeSpan.FromMinutes(5));
+            cache.Set(Key, currencyResponse, cacheOptions);
         }
 
         return currencyResponse!;
@@ -33,12 +32,12 @@ public class CurrencyService(HttpClient client, IMemoryCache cache) : ICurrencyE
     public async Task<Money> ConvertToAsync(Money money, Currency currency)
     {
         if (money.Currency == currency) return money;
-        
-        var data = await GetCurrencyExchange();
-        decimal rateFrom = Convert.ToDecimal(data.Rates[money.Currency]);
-        decimal rateTo = Convert.ToDecimal(data.Rates[currency]);
 
-        decimal converted = (money.Amount / rateFrom) * rateTo;
+        var data = await GetCurrencyExchange();
+        var rateFrom = Convert.ToDecimal(data.Rates[money.Currency]);
+        var rateTo = Convert.ToDecimal(data.Rates[currency]);
+
+        var converted = (money.Amount / rateFrom) * rateTo;
         return new Money(currency, converted);
     }
 }
