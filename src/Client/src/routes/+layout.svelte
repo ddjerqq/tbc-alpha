f0e699d1865cf5b7c319ded98fa699b923f31acf
ddjerqq <57017344+ddjerqq@users.noreply.github.com>
2024-10-20 13:00:25 +0400

    added stuff

--- src/Client/src/routes/+layout.svelte
+++ src/Client/src/routes/+layout.svelte
@@ -9,7 +9,12 @@
 
 <div class="w-full h-full bg-topo overflow-y-scroll flex flex-col justify-between px-5 bg-neutral-100">
     <main class="my-10 h-[calc(100% + 10rem)] flex flex-col justify-center align-middle items-center">
-        <slot></slot>
+
+        <div class="w-full flex justify-center">
+            <div class="w-full max-w-[400px] flex flex-col gap-2">
+                <slot></slot>
+            </div>
+        </div>
     </main>
 
     <NavBar/>
